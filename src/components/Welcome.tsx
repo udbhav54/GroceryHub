313d5cfa25364f7d1e27ca93a3ef0bda23bd33de
Udbhav <udbhavkumar54@gmail.com>
2025-12-29 22:42:14 +0530

    welcome page, motion lib and animation lucide lib set-up

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,88 @@
+'use client'
+import React from 'react'
+import { motion } from "motion/react";
+import { ArrowRight, Bike, ShoppingBasket } from 'lucide-react';
+
+function Welcome() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-6">
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.6,
+        }}
+        className='flex items-center gap-3'
+      >
+        <ShoppingBasket className='w-11 h-11 text-green-600'/>
+        <h1 className="text-4xl md:text-5xl font-extrabold text-green-700">
+          GroceryHub
+        </h1>
+      </motion.div>
+
+        <motion.p
+        initial={{
+          opacity:0,
+          y:10
+        }}
+        animate={{
+          opacity: 1,
+          y: 0
+        }}
+        transition={{
+          duration: 0.6,
+          delay:0.3
+        }}
+        className='mt-4 text-gray-700 text-lg md:text-xl max-w-lg'
+        >
+          Your one-stop destination for fresh groceries, organic produce, and daily essentials delivered straight to your doorstep.
+        </motion.p>
+
+        <motion.div
+          initial={{
+            opacity:0,
+            scale:0.9
+          }}
+          animate={{
+            opacity:1,
+            scale:1
+          }}
+          transition={{
+            duration:0.6,
+            delay:0.5
+          }}
+          className='flex items-centr justify-center gap-10 mt-10'
+          >
+            <ShoppingBasket className='w-20 h-24 md:w-32 md:h-32 text-green-600'/>
+            <Bike className='w-24 h-24 md:w-32 md:h-32 text-orange-500'/>
+        </motion.div>
+
+        <motion.button
+        initial={{
+          opacity:0,
+          y:20
+        }}
+        animate={{
+          opacity:1,
+          y:0
+        }}
+        transition={{
+          duration:0.6,
+          delay:0.8
+        }}
+        className='inline-flex items-center gap-2 bg-green-600 hover:bg-green-700 text-white font-semibold py-3 px-8 rounded-2xl shadow-md transition-all duration-200 mt-10'>
+          Next
+          <ArrowRight />
+        </motion.button>
+
+    </div>
+  );
+}
+
+export default Welcome
