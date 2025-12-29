41a4a3ba303610ba4e63fd7f54d950e50e980174
Udbhav <udbhavkumar54@gmail.com>
2025-12-29 20:49:03 +0530

    auth-sign set-up

---
+++ src/auth.ts
@@ -0,0 +1,68 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import connectDb from "./lib/db";
+import User from "./models/user.model";
+import bcrypt from "bcryptjs";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, request) {
+        
+          await connectDb()
+          const email = credentials.email
+          const password = credentials.password as string
+          const user = await User.findOne({ email })
+          if (!user) {
+            throw new Error("No user found")
+          }
+          const isMatch = await bcrypt.compare(password, user.password)
+          if(!isMatch) {
+            throw new Error("Invalid password")
+          }
+
+          return {
+            id:user._id.toString(),
+            email: user.email,
+            name: user.name,
+            role: user.role
+          }
+      }
+    }),
+  ],
+  callbacks: {
+    // put data inside token
+     jwt({token, user}){
+      if (user) {
+        token.id = user.id
+        token.name = user.name
+        token.email = user.email
+        token.role = user.role
+      }
+      return token
+    },
+    // put data inside session
+    session({session, token}) {
+      if (session.user) {
+        session.user.id = token.id as string
+        session.user.email = token.email as string
+        session.user.name = token.name as string
+        session.user.role = token.role as string
+      }
+      return session
+    },
+  },
+  pages: {
+    signIn: "/login",
+    error: "/login"
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 10 * 24 * 60 * 60 * 1000 // 10 days
+  },
+  secret:process.env.AUTH_SECRET
+})
